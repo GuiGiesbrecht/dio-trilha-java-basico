6e3c6e37b1cccb5767f86509fa8e707c3a710470
Guilherme Duarte Giesbrecht <guidgiesbrecht@gmail.com>
2023-12-04 19:48:58 -0300

    Simple console data request

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int numero;
+        String agencia;
+        String nomeCliente;
+        double saldo;
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Digite o nome do titular: ");
+            nomeCliente = scanner.nextLine();
+
+            System.out.println("Digite o número da conta: ");
+            numero = scanner.nextInt();
+
+            System.out.println("Digite a agência: ");
+            agencia = scanner.nextLine();
+
+            System.out.println("Digite o saldo: ");
+            saldo = scanner.nextDouble();
+        } catch (Exception e) {
+            System.out.println("Erro ao ler dados da conta.");
+            throw e;
+        }
+
+        System.out.println(
+                "Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia
+                        + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
     }
 }
